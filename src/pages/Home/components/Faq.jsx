ba520a1e955efa55b5f9e5d0db202b98737548b3
Ebiperre Iyoro <iyorop@yahoo.com>
2023-02-09 14:33:46 +0100

    Feat: update on faq

--- src/pages/Home/components/Faq.jsx
+++ src/pages/Home/components/Faq.jsx
@@ -8,7 +8,7 @@ const Faq = () => {
         <div className="flex flex-col gap-12 mt-5 ">
           {/* title */}
           <div className="flex flex-col items-center justify-center ml-36 sm:ml-5">
-            <span className="text-5xl font-semibold">Do you still have questions?</span>
+            <span className="text-5xl font-semibold sm:ml-0 -ml-11">Do you still have questions?</span>
             <span className=" text-xl mt-6 my-3">
               Check out our FAQ page to answer any of your questions around our
               products and services, and if you require further assistance or
